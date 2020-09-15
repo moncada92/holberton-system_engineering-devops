1066d7804d32c31e540af39486052122679e8d4d
Camilo Moncada <monk-da@hotmail.com>
2020-09-14 21:53:07 -0500

    response question 0 at 3

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""reques API"""
+
+import requests
+from sys import argv
+
+
+def getData(id):
+    '''get data of employee'''
+    URL = 'https://jsonplaceholder.typicode.com/'
+    ENDPOINT = URL + 'users/{}'.format(id)
+    employee = requests.get(ENDPOINT).json()
+    TASKENDPOINT = URL + 'todos?userId={}'.format(employee.get('id'))
+    tasks = requests.get(TASKENDPOINT).json()
+    data = {"employee": employee, "tasks": tasks}
+    total = len(data['tasks'])
+    tasks = [task for task in data['tasks'] if task['completed']]
+    completed = len(tasks)
+    print('Employee {} is done with tasks({}/{}):'
+          .format(data['employee']['name'], completed, total))
+    for task in tasks:
+        print('\t {}'.format(task['title']))
+
+
+if __name__ == '__main__':
+    getData(argv[1])
+
---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""export to JSON"""
+
+import json
+import requests
+from sys import argv
+
+
+def getData(id):
+    '''get data  adn export of employee'''
+    URL = 'https://jsonplaceholder.typicode.com/'
+    ENDPOINT = URL + 'users/{}'.format(id)
+    employee = requests.get(ENDPOINT).json()
+    TASKENDPOINT = URL + 'todos?userId={}'.format(employee['id'])
+    tasks = requests.get(TASKENDPOINT).json()
+    data = {"employee": employee, "tasks": tasks}
+    _id = data['employee']['id']
+    _allData = []
+    for task in data['tasks']:
+        employee = {}
+        employee['task'] = task['title']
+        employee['completed'] = task['completed']
+        employee['username'] = data['employee']['username']
+        _allData.append(employee)
+    _final = {_id: _allData}
+    with open('{}.json'.format(_id), 'w') as f:
+        json.dump(_final, f)
+
+
+if __name__ == '__main__':
+    getData(argv[1])
