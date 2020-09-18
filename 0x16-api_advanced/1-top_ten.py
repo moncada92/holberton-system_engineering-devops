fa8e011c25e1a8e57f44ac8e7ff62a1bb7b6ed80
Camilo Moncada <monk-da@hotmail.com>
2020-09-17 23:55:18 -0500

    response question 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+
+#!/usr/bin/python3
+'''top ten in reddit'''
+
+
+import requests
+
+
+def top_ten(subreddit):
+    '''get the top 10'''
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+            (KHTML, like Gecko; Google Web Preview) \
+            Chrome/27.0.1453 Safari/537.36"
+    headers = {"User-Agent": agent}
+    response = requests.get(url, headers=headers).json()
+    if 'error' in response:
+        print('None')
+        return
+    _top = response['data']['children']
+    for i, top in enumerate(_top[:10], 1):
+        print(top['data']['title'])
